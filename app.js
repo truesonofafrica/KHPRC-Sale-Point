3ffbedd57af82e887989d5c8f21ea2818ec3b5fa
truesonofafrica <kwesiamissah020@gmail.com>
2025-07-28 21:51:08 +0000

    swagger docs added

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { rateLimit } = require('express-rate-limit');
 
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const notFound = require('./utils/notFound');
 const errorHandler = require('./controllers/errorController');
 
@@ -21,6 +26,13 @@ app.use(
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/salepoints', salePointRouter);
 app.use(notFound);
 app.use(errorHandler);
